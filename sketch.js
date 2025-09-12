a150a4a43bc0b55a5816933b9c14df1f696fa703
Your Name <fishpenguin105@gmail.com>
2025-09-12 09:14:53 -0700

    Game done

--- sketch.js
+++ sketch.js
@@ -30,6 +30,7 @@ function draw() {
   if (keyIsDown(37) && shipX >= 20 || keyIsDown(65) && shipX >= 20) {
     shipX -= 5;
   }
+
   if (keyIsDown(39) && shipX <= 380 || keyIsDown(68) && shipX <= 380) {
     shipX += 5;
   }
@@ -37,18 +38,47 @@ function draw() {
   if (mX < shipX + 22.5 && mX > shipX - 22.5 && mY > 350) {
     collision = true;
     score = 0;
-    lives -= 1;
     background('red');
+  } if (collision === true) {
+    lives -= 1;
   }
-}
-if (mY === 400) {
-  mY = 20
-  mX = random(20, 380);
-  score += 1;
+
+  if (mY === 400) {
+    mY = 20
+    mX = random(20, 380);
+    score += 1;
+    collision = false;
+  }
+
+  if (collision === true) {
+    mY = 20
+    mX = random(20, 380);
+    score += 1;
+    collision = false;
+  }
 
   fill('white');
   text("Score: " + score, 10, 20);
-  text("Lives: " + lives, 10, 35)
+  text("Lives: " + lives, 10, 35);
+
+  if (lives === 0) {
+    fill(random(1, 255), 0, 0);
+    background(30);
+    textSize(40);
+    text("You lose!", 125, 200);
+    textSize(20);
+    text("Press R to restart", 125, 250)
+    shipX = 500;
+  }
+
+  if (keyCode === 82 && keyIsPressed) {
+    background(30);
+    shipX = 200;
+    lives = 3;
+    textSize(0);
+    score = 0;
+    mY = -10;
+  }
 }
 
 function drawMeteor() {
@@ -59,7 +89,7 @@ function drawMeteor() {
 function drawShip(x) {
   fill('blue');
   ellipse(x, 345, 20, 30);
-  
+
   fill('white');
   rect(x, 360, 20, 30);
 
